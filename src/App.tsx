83f6abea7071a531fa63bc85727adae4cacc410f
asdasd <92120487+Ich-mag-dich@users.noreply.github.com>
2023-02-19 23:53:34 +0900

    add bookmark

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,28 @@
-import React, { useState, useEffect } from 'react';
-import Axios from 'axios';
-import './App.css';
-import WeatherDiv from './components/weatherComponent';
-import CityDiv from './components/cityComponent';
-import { setSelectionRange } from '@testing-library/user-event/dist/utils';
+import React, { useState, useEffect, useCallback } from "react";
+import Axios from "axios";
+import "./App.css";
+import WeatherDiv from "./components/weatherComponent";
+import CityDiv from "./components/cityComponent";
+import { Cookies } from "react-cookie";
+import { count } from "console";
 
 type ObjType = {
-  [index: string]: string
-  "01d": string
-  "01n": string
-  "02d": string
-  "02n": string
-  "03d": string
-  "03n": string
-  "04d": string
-  "04n": string
-  "09d": string
-  "09n": string
-  "10d": string
-  "10n": string
-  "11d": string
-  "11n": string
-}
+  [index: string]: string;
+  "01d": string;
+  "01n": string;
+  "02d": string;
+  "02n": string;
+  "03d": string;
+  "03n": string;
+  "04d": string;
+  "04n": string;
+  "09d": string;
+  "09n": string;
+  "10d": string;
+  "10n": string;
+  "11d": string;
+  "11n": string;
+};
 
 export const WeatherIcons: ObjType = {
   "01d": "icon/sunny.svg",
@@ -41,71 +42,143 @@ export const WeatherIcons: ObjType = {
   "50n": "icon/cloudy.svg",
 };
 
+var countNum = 1;
+
 function App() {
+  countNum = 1;
+  console.log("rendering");
+  let cookieBool = false;
   const [city, setCity] = useState();
-  const [weather, setWeather] = useState();
-  // useEffect(() => {
-  //   console.log(city);
-  // }, [city]);
-  // useEffect(() => {
-  //   console.log(weather);
-  // }, [weather])
+  const [weather, setWeather]: any = useState();
+  const [, updateState] = useState({});
+  // console.log("q "+city);
+  // console.log("q "+weather);
+  useEffect(() => {
+    // console.log(city)
+  }, [city]);
+  useEffect(() => {
+    // console.log(weather)
+  }, [weather]);
+  const forceUpadte = useCallback(() => updateState({}), []);
+  const getBookmark = () => {
+    const cookies = new Cookies();
+    try {
+      if (cookies.get("city").length > 0) {
+        cookieBool = true;
+      } else {
+        cookieBool = false;
+      }
+    } catch (e) {}
+  };
+  getBookmark();
+
   const getWeather = async (e: any) => {
     if (city !== undefined) {
-
-      e.preventDefault();
+      // e.preventDefault();
       const response = await Axios.get(
-        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=72dc22879afa657a9417a3eb73526904`,
-      );
-      return response.data
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=72dc22879afa657a9417a3eb73526904`
+      ).then((e) => {
+        forceUpadte();
+        return e;
+      });
+      return response.data;
     }
   };
   const handleChange = (event: any) => {
     event.preventDefault();
-    setCity(event.target.value)
-  }
+    setCity(event.target.value);
+  };
   const handleSubmit = async (event: any) => {
-    event.preventDefault();
+    try {
+      event.preventDefault();
+    } catch {}
     try {
       setWeather(await getWeather(event));
-    } catch (e) { }
-  }
+    } catch (e) {}
+  };
+  const cookies = new Cookies();
+
+  const reanderingB = async (city: any) => {
+    await setCity(city);
+    // setWeather(await getWeather(city))
+  };
+  const renderingA = () => {
+    const result = [];
+    for (const i of cookies.get("city")) {
+      result.push(
+        <span
+          className="fav"
+          onClick={
+            // eslint-disable-next-line no-loop-func
+            (e: any) => {
+              console.log(countNum);
+              // console.log(e.target.innerText);
+              // console.log(city);
+
+              setCity(i);
+              if (countNum === 1) {
+                // console.log(e);
+                handleSubmit(i);
+                document.querySelectorAll(".fav").forEach((e: any) => {
+                  // console.log(`e${e.innerHTML}, i ${i}`);
+                  // console.log(e.innerHTML === ` ${i} `)
+                  if (e.innerHTML === ` ${i} `) {
+                    // console.log("asd");
+                    if (i !== weather?.name) {
+                      setTimeout(() => {
+                        e.click();
+                        countNum = 0;
+                      }, 100);
+                    }
+                  }
+                });
+              }
+            }
+          }
+        >
+          {" "}
+          {i}{" "}
+        </span>
+      );
+    }
+    return result;
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <div className="box">
-
+        <div className="box box3">
           <p>
-            <code className='code' >
-              <a title="" className="button btnPush btnLightBlue" onClick={() => {
-                setCity(undefined);
-                setWeather(undefined);
-              }}>{city && weather ? ("Clear!") : ("Check The Weather!")}</a>
-              {/* <button onClick={() => {
-                setCity(undefined);
-                setWeather(undefined);
-              }}>
-                Check the Weather!
-              </button> */}
+            <code className="code">
+              <a
+                title=""
+                className="button btnPush btnLightBlue"
+                onClick={() => {
+                  setCity(undefined);
+                  setWeather(undefined);
+                }}
+              >
+                {city && weather ? "Clear!" : "Check The Weather!"}
+              </a>
             </code>
           </p>
-          {/* <form onSubmit={handleSubmit}>
-          <label>
-          City:
-            <input type="text" onChange={handleChange} />
-            </label>
-            <input type="submit" value="Submit" />
-          </form> */}
           <div>
             {city && weather ? (
               <div>
                 <WeatherDiv weather={weather} />
               </div>
             ) : (
-              <CityDiv handleSubmit={handleSubmit} handleChange={handleChange} />
+              <CityDiv
+                handleSubmit={handleSubmit}
+                handleChange={handleChange}
+              />
             )}
           </div>
         </div>
+        {cookieBool ? (
+          <div className="box box2">{renderingA()}</div>
+        ) : (
+          <div></div>
+        )}
       </header>
     </div>
   );
--- src/components/weatherComponent.tsx
+++ src/components/weatherComponent.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
-import './css/weatherCp.css';
+import React, { useCallback, useState } from "react";
+import "./css/weatherCp.css";
 import { WeatherIcons } from "../App";
+import { Cookies } from "react-cookie";
+
+const cookies = new Cookies();
 
 export const WeatherInfoIcons = {
   sunset: "my-weather-app/icon/temp.svg",
@@ -11,8 +14,8 @@ export const WeatherInfoIcons = {
 };
 
 const marginAutoLeft = {
-  margin: 'auto',
-  marginLeft: '10px',
+  margin: "auto",
+  marginLeft: "10px",
 };
 
 function WeatherDiv(props: any) {
@@ -21,49 +24,96 @@ function WeatherDiv(props: any) {
   const unixTimeSr = weather?.sys?.sunrise;
   const unixTimeSs = weather?.sys?.sunset;
   const dateSr = new Date(unixTimeSr * 1000);
-  const dateSs = new Date(unixTimeSs * 1000)
+  const dateSs = new Date(unixTimeSs * 1000);
 
   const iconname: string = weather.weather[0].icon as string;
   return (
-    <div className='weatherCpBox'>
-      <div className='nameBox'>
+    <div className="weatherCpBox">
+      <div className="nameBox">
         <span className="temp1">
-          <span className="temp2">{`${Math.floor(weather?.main?.temp - 273)}°C`}</span> | {weather?.weather[0]?.description}</span>
-        <img className="weatherImg" src={WeatherIcons[iconname]} />
+          <span className="temp2">{`${Math.floor(
+            weather?.main?.temp - 273
+          )}°C`}</span>{" "}
+          | {weather?.weather[0]?.description}
+        </span>
+        <img alt="" className="weatherImg" src={WeatherIcons[iconname]} />
       </div>
-      <span className="nameCountry">{weather?.name}, {weather?.sys?.country}</span>
+      <span className="nameCountry">
+        {weather?.name}, {weather?.sys?.country}
+        <button
+          className="bookmark"
+          onClick={() => {
+            let oldCookie: any[] = [];
+            try {
+              for (const i of cookies.get("city")) {
+                oldCookie.push(i);
+              }
+            } catch (e) {
+              console.log(e);
+            }
+            if (!oldCookie.includes(weather?.name)) {
+              oldCookie.push(weather?.name);
+              alert("즐겨찾기에 추가가 완료되었습니다.");
+            } else {
+              const indexA = oldCookie.indexOf(weather?.name);
+              if (indexA > -1) {
+                oldCookie.splice(indexA, 1);
+                document.querySelectorAll(".fav").forEach((i: any) => {
+                  if (i.innerText === weather?.name) {
+                    i.remove();
+                  }
+                });
+              }
+            }
+
+            // oldCookie.push(cookies.get("city"))
+            cookies.set("city", oldCookie);
+            console.log(cookies.get("city"));
+          }}
+        >
+          Bookmark
+        </button>
+      </span>
       <span className="weatherinfo">Weather Info</span>
 
       <div className="divBox">
         <div className="inBoxDiv">
-          <img className="icons" src={"icon/temp.svg"} />
+          <img alt="" className="icons" src={"icon/temp.svg"} />
           <span className="inSpan" style={marginAutoLeft}>
-            <span className="ininSpan"> {dateSr.getHours()} : {dateSr.getMinutes()} | Sunrise</span>
-            <span className="ininSpan"> {dateSs.getHours()} : {dateSs.getMinutes()} | Sunset</span>
+            <span className="ininSpan">
+              {" "}
+              {dateSr.getHours()} : {dateSr.getMinutes()} | Sunrise
+            </span>
+            <span className="ininSpan">
+              {" "}
+              {dateSs.getHours()} : {dateSs.getMinutes()} | Sunset
+            </span>
           </span>
         </div>
         <div className="inBoxDiv">
-
-          <img className="icons" src={"icon/humidity.svg"} />
-          <span className="inSpan">{weather?.main?.humidity}
+          <img alt="" className="icons" src={"icon/humidity.svg"} />
+          <span className="inSpan">
+            {weather?.main?.humidity}
             <span className="ininSpan">Humidity</span>
           </span>
         </div>
         <div className="inBoxDiv">
-          <img className="icons" src={"icon/wind.svg"} />
-          <span className="inSpan">{weather?.wind?.speed}
+          <img alt="" className="icons" src={"icon/wind.svg"} />
+          <span className="inSpan">
+            {weather?.wind?.speed}
             <span className="ininSpan">Wind Speed</span>
           </span>
         </div>
         <div className="inBoxDiv">
-          <img className="icons" src={"icon/pressure.svg"} />
-          <span className="inSpan">{weather?.main?.pressure}
+          <img alt="" className="icons" src={"icon/pressure.svg"} />
+          <span className="inSpan">
+            {weather?.main?.pressure}
             <span className="ininSpan">Pressure</span>
           </span>
         </div>
       </div>
-    </div >
-  )
+    </div>
+  );
 }
 
 export default WeatherDiv;
